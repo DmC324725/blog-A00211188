02421b7d1886b13a7fff8f34d6d5e13204df7354
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-07-02 11:17:03 -0400

    commit

--- private/app/views/template/top.php
+++ private/app/views/template/top.php
@@ -1,7 +1,12 @@
 <!-- Top Part Start -->
 
 <body>
+    <nav>
+    <a href="/////////">Link to home page </a>
+    <a href="/////////">Link to blog </a>
+    <a href="/////////">Link to listing </a>
 
+</nav>
 
 
     <h1>This is the top part of my html body</h1>
