08ed728ad84c71b5a6ca7261756ff7a333ec8ace
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-07-06 19:36:47 -0400

    commit

--- private/app/views/home/home-body.php
+++ private/app/views/home/home-body.php
@@ -92,7 +92,7 @@
                         irure velit commodo cillum sit nulla ullamco magna amet magna cupidatat qui labore cillum sit in tempor 
                         veniam consequat non laborum adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa 
                         occaecat ut laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in 
-                        adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed..
+                        adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.
                         </p>
                     </div> 
 
