5f97894350e0dae84b9d833802c1e198be10bc9c
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-07-23 15:23:21 -0400

    commit

---
+++ private/app/models/blogmodel.php
@@ -0,0 +1,13 @@
+class BlogModel extends Model{
+    function __construct() {
+        parent::__construct();
+    }
+
+    function DbVersion() {
+        $sql = 'SELECT VERSION()';
+        $stmt = this->db->query($sql);
+        $res = $stmt->fetch();
+
+        return $res[0];
+    }
+}                           
