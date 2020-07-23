adccca90f3eb8bb7a078390176920247fe36c5f8
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-07-23 17:23:00 -0400

    commit

--- setup/data/init.sql
+++ setup/data/init.sql
@@ -1,16 +1,16 @@
 use database;
-CREATE TABLE `author` (
-`email` varchar(255) primary key
-, `name` varchar(255) not null
-, `password_hash` varchar(255) not null
+CREATE TABLE author (
+email varchar(255) primary key
+, name varchar(255) not null
+, password_hash varchar(255) not null
 );
 
-CREATE TABLE 'blog_post' (
-    'slug' varchar(40)
-    , 'post_name' varchar(255)
-    , 'post_content' text
-    , 'author_email' varchar(255) REFERENCES author('email') 
-    , 'publish_date' date NOT NULL
+CREATE TABLE blog_post (
+    slug varchar(40) primary key
+    , post_name varchar(255)
+    , post_content text
+    , author_email varchar(255) REFERENCES author(email) 
+    , publish_date date NOT NULL
 
 );
 ------ Blog Post Fields
