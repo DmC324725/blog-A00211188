3644ea2ae9b5c78e361d7c4a53e4a02734c756e0
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-17 14:02:57 -0400

    commit

--- private/app/views/newpost/newpost-body.php
+++ private/app/views/newpost/newpost-body.php
@@ -1,9 +1,9 @@
 <form>
 
-<input type = "text" name = "post-title" id="post-title" placeholder="Post Title"/>
+<input type = "text" class="newinput" name = "post-title" id="post-title" placeholder="Post Title"/>
 <br/>
 <!-- <label for="post-content">:</label> -->
-<textarea id="post-content" name="post-content" rows="30" cols="150" placeholder="Post Content"></textarea>
+<textarea id="post-content" class="newinput" name="post-content" rows="30" cols="150" placeholder="Post Content"></textarea>
 <br/>
 <input type = "submit" value="Post" id="submit-post" name = "submit-post"/>
 
