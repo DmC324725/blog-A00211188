acf2278e86cba6135f2aa1f64637f364bb72c37d
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-17 14:22:38 -0400

    ciommit

--- private/app/views/newpost/newpost-body.php
+++ private/app/views/newpost/newpost-body.php
@@ -5,7 +5,7 @@
 <!-- <label for="post-content">:</label> -->
 <textarea id="post-content" class="newinput" name="post-content" rows="30" cols="150" style = "width:100%;" placeholder="Post Content"></textarea>
 <br/>
-<div style="display:flex; justify-content:center;">
+<div style="width:100%;display:flex; justify-content:center;">
 <input type = "submit" value="Post" id="submit-post" name = "submit-post" />
 <div>
 </form>
