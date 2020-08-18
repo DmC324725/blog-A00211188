265eadce8737b45be1e344c2f8263050de894e29
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-18 11:20:00 -0400

    commit

--- private/app/views/newpost/newpost-body.php
+++ private/app/views/newpost/newpost-body.php
@@ -19,7 +19,7 @@
         $post_content = $_POST['post-content'];
 
         $slug = str_replace(' ','_',$post_title);
-        $slug = $slug + rand(1000,9999);
+        $slug = $slug . rand(1000,9999);
 
         $postVals = array($slug,$post_title,$post_content,'static@email.com');
         print_r($postVals);
