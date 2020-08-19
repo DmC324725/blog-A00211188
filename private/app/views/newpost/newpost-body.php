05acc50c5a48c1c66aa8a6dec70afe260685e574
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-19 00:27:03 -0400

    commit

--- private/app/views/newpost/newpost-body.php
+++ private/app/views/newpost/newpost-body.php
@@ -1,10 +1,10 @@
 <?php echo "<script type='text/javascript'>console.log('Page Load Started');</script>";?>
 <form method = "post">
 
-<input type = "text" class="newinput" name = "post-title" id="post-title" style = "width:100%;" placeholder="Post Title" value = "<?php if(is_string($post_name)){echo "$post_name"; $post_name="";} ?>"/>
+<input type = "text" class="newinput" name = "post-title" id="post-title" style = "width:100%;" placeholder="Post Title" value = "<?php if(is_string($post_name)){echo "$post_name";} ?>"/>
 <br/>
 <!-- <label for="post-content">:</label> -->
-<textarea id="post-content" class="newinput" name="post-content" rows="30" cols="150" style = "width:100%;" placeholder="Post Content"><?php if(is_string($post_content)){echo "$post_content";$post_content="";} ?></textarea>
+<textarea id="post-content" class="newinput" name="post-content" rows="30" cols="150" style = "width:100%;" placeholder="Post Content"><?php if(is_string($post_content)){echo "$post_content";} ?></textarea>
 <br/>
 <div style="width:100%;display:flex; justify-content:center;">
 <input type = "submit" value="Post" name = "submit" />
@@ -24,14 +24,12 @@
             echo "<script type='text/javascript'>console.log('True Condition Ran');</script>";
             $this->blogmodel->updateBlogPost($slug,$edit_post_title,$edit_post_content);
 
-            //Unsetting Variables
-            unset($_POST, $slug, $post_name, $post_content);
-
         }else{
             echo "<script type='text/javascript'>console.log('False Condition Ran');</script>";
             $edit_slug = str_replace(' ','_',$edit_post_title);
             $edit_slug = substr($edit_slug,0,20) . rand(1000,9999);
             $postVals = array($edit_slug,$edit_post_title,$edit_post_content,'static@email.com');
+            $this->model('blogmodel');
             $this->blogmodel->insertBlogPost($postVals);
         }
        
