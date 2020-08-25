ca98f232df9e25f6be4924acb5af82b9b874295f
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-25 14:19:41 -0400

    commit

--- private/app/views/signup/signup-body.php
+++ private/app/views/signup/signup-body.php
@@ -7,13 +7,13 @@
 <div style="width:100%;display:flex; justify-content:center;">
     <h2>Sign Up</h2>
     <input type = "text" class="newinput" name = "signup-user-name" id="signup-user-name"  placeholder="Email" />
-    <br/>
+    <br>
     <input type = "email" class="newinput" name = "signup-user-email" id="signup-user-email"  placeholder="Email" />
-    <br/>
+    <br>
     <input type = "password" class="newinput" name = "signup-user-pass" placeholder="password" id="signup-user-pass"   />
-    <br/>
+    <br>
     <input type = "password" class="newinput" name = "signup-user-pass-repeat" placeholder="password" id="signup-user-pass-repeat"   />
-    <br/>
+    <br>
     <input type = "submit" value="Sign Up" name = "submit" />
 </div>
 
