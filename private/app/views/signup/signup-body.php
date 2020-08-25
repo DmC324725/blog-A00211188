f14f1c64c20972abfd6b5af5fe547f7fa85729d4
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-25 13:47:45 -0400

    commit

---
+++ private/app/views/signup/signup-body.php
@@ -0,0 +1,42 @@
+<?php echo "<script type='text/javascript'>console.log('Page Load Started');</script>"; ?>
+
+
+<form method = "post">
+
+
+<div style="width:100%;display:flex; justify-content:center;">
+<label for="login-user-email">Email: </label>
+<input type = "email" class="newinput" name = "login-user-email" id="login-user-email"  placeholder="johndoe@email.com" />
+<br/>
+</div>
+<div style="width:100%;display:flex; justify-content:center;">
+<label for="login-user-password">Password: </label>
+<input type = "password" class="newinput" name = "login-user-pass" id="login-user-pass"   />
+<br/>
+</div>
+<div style="width:100%;display:flex; justify-content:center;">
+<input type = "submit" value="Login" name = "submit" />
+</div>
+</form>
+
+<?php echo "<script type='text/javascript'>console.log('Page Load Complete');</script>";?>
+
+<?php 
+    
+    if(isset($_POST['submit'])){
+        //Get the data and run SQL Statement to insert into 
+        $edit_post_title = $_POST['post-title'];        
+        $edit_post_content = $_POST['post-content'];
+        if(is_string($slug) & strlen($slug)>3){
+            $this->blogmodel->updateBlogPost($slug,$edit_post_title,$edit_post_content);
+
+        }else{
+            $edit_slug = str_replace(' ','_',$edit_post_title);
+            $edit_slug = substr($edit_slug,0,20) . rand(1000,9999);
+            $postVals = array($edit_slug,$edit_post_title,$edit_post_content,'static@email.com');
+            $this->model('blogmodel');
+            $this->blogmodel->insertBlogPost($postVals);
+        }
+    }
+
+?>
