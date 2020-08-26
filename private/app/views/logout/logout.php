9b288bb42c0c399cc3755d6e0446789dbeedb506
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-26 04:59:11 -0400

    commit

--- private/app/views/logout/logout.php
+++ private/app/views/logout/logout.php
@@ -4,9 +4,7 @@
     $_SESSION["logged_in"] = "";
     $_SESSION["name"] = "";
     $_SESSION["email"] = "";
-    session_unset($_SESSION["logged_in"]);
-    session_unset($_SESSION["name"]);
-    session_unset($_SESSION["email"]);
+    session_unset();
     
     echo "You have been successfully logged out! <br>";
     echo "<a href='/'><input type='button' value='Go Back'/></a>'";
