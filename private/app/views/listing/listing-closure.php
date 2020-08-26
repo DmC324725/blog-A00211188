9920c632cd4ba517cd0ef36fea48034b4d8ceb45
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-26 05:14:10 -0400

    commit

--- private/app/views/listing/listing-closure.php
+++ private/app/views/listing/listing-closure.php
@@ -1,8 +1,15 @@
 
            </div> <!-- end main -->
 </div> <!-- end row -->
+<?php 
+    if(isset($_SESSION["logged_in"]) && $_SESSION["logged_in"] === true){
 
-<div style="display:flex;justify-content:center;"><a href="/main/new_post"><input type = "button" value="New Post"/></a></div>
+        echo "<div style='display:flex;justify-content:center;'><a href='/main/new_post'><input type = 'button' value='New Post'/></a></div>";
+        return;
+
+    }
+
+?>
 
 </div> <!-- end content-wrap -->
 
