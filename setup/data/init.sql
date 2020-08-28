faea0c1be7ef240f47301108b6687ece09e1f918
Faisal Akram Khan <faisalkhanfk.fk31@gmail.com>
2020-08-28 16:13:13 -0400

    commit

--- setup/data/init.sql
+++ setup/data/init.sql
@@ -28,3 +28,14 @@ INSERT INTO blog_posts values ("narutoslug", "How to become a Hokage",  "This is
 INSERT INTO blog_posts values ("luffyslug", "How to become The King of the Pirates", "This is just a sample post content " , "luffy@gmail.com", now());
 INSERT INTO blog_posts values ("luffy2slug", "How to season meat", "This is just a sample post content " , "luffy@gmail.com", now());
 
+INSERT INTO blog_posts values ("johndoesasdkj2", "This is post number six", "This is just a sample post content " , "johndoe@gmail.com", now());
+INSERT INTO blog_posts values ("johndoe2slugasdascj", "This is post number seven", "This is just a sample post content " , "johndoe@gmail.com", now());
+INSERT INTO blog_posts values ("narutoslu21eadg", "This is post number eight",  "This is just a sample post content " ,"naruto@gmail.com", now());
+INSERT INTO blog_posts values ("luffyslugsdc2f", "This is post number nine", "This is just a sample post content " , "luffy@gmail.com", now());
+INSERT INTO blog_posts values ("luffy2slugcx3fsdv2", "This is post number ten", "This is just a sample post content " , "luffy@gmail.com", now());
+
+INSERT INTO blog_posts values ("johndoeslugzxcq12v", "This is post number eleven", "This is just a sample post content " , "johndoe@gmail.com", now());
+INSERT INTO blog_posts values ("johndoe2slugxcza21", "This is post number twelve", "This is just a sample post content " , "johndoe@gmail.com", now());
+INSERT INTO blog_posts values ("narutoslugasfsg41", "This is post number thirteen",  "This is just a sample post content " ,"naruto@gmail.com", now());
+INSERT INTO blog_posts values ("luffyslugsdvser32", "This is post number fourteen", "This is just a sample post content " , "luffy@gmail.com", now());
+INSERT INTO blog_posts values ("luffy2swlugsadq", "This is post number fifteen... I'm tired now!", "This is just a sample post content " , "luffy@gmail.com", now());
